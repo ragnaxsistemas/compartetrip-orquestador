35f8df0629b03666126c600206d63bfc6500fc44
juliocornejo <elimino.gente.por.dinero@gmail.com>
2019-12-15 18:49:57 -0300

    first commit

---
+++ src/main/java/com/ragnax/compartetriporquestador/CompartetripOrquestadorApplication.java
@@ -0,0 +1,13 @@
+package com.ragnax.compartetriporquestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompartetripOrquestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompartetripOrquestadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnax/compartetriporquestador/CompartetripOrquestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnax.compartetriporquestador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompartetripOrquestadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
